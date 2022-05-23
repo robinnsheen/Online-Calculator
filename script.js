6962d75bb96d1f45a246be59be8c523e7ae65f61
Robin Sheen <robinnsheen@berkeley.edu>
2022-05-23 03:02:14 -0700

    linked functions to digits operators clear and equal. currently unable to chain equations without equal sign between pairs

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
-const digits = document.querySelectorAll(".digit");
-const operations = document.querySelectorAll(".operators");
+/**
+ * //digit function
+ * pressing a button assigns button value to x
+ * pressing second button multiplies first value by 10
+ * and adds second button value, etc
+ *
+ * continue until operator is pressed
+ * return x and operator.
+ *
+ * store return values in x and operator
+ *
+ * //end digit function
+ *
+ * //2nd digit function
+ * pressing a button assigns button value to x
+ * pressing second button multiplies first value by 10
+ * and adds second button value, etc
+ *
+ * continue until equal is pressed
+ * return y and equal sign
+ *
+ * //end digit function
+ *
+ *
+*/
 
-function printDigits() {
-  for (let digit of digits) {
-    console.log(digit.innerText);
-  }
+
+const digits = document.querySelectorAll(".digit");
+const operations = document.querySelectorAll(".operator");
+let x;
+let y;
+let operator;
+
+/** Create clear button. */
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", clearValues);
+
+function clearValues() {
+  x = undefined;
+  y = undefined;
+  operator = undefined;
+
 }
 
 /** Create digit buttons */
@@ -14,46 +49,90 @@ for (let digit of digits) {
 }
 
 //handle clicking on a digit//
+function handleDigitClick(evt) {
+  let digitValue = evt.currentTarget.innerText;
+  console.log(digitValue);
+  if (x && !operator) {
+    x += digitValue;
+  }
 
-function handleDigitClick() {
+  if (!x && !operator)
+  {
+    x = digitValue;
+  }
+
+  if (operator && y) {
+    y += digitValue;
+  }
+
+  if (operator && !y) {
+    y = digitValue;
+  }
 
 }
 
-function add(x, y) {
-  return x + y;
+/** Create operation buttons */
+
+for (let operation of operations) {
+  operation.addEventListener("click", handleOperationClick);
 }
 
-function subtract(x, y) {
+//handle clicking on an operator//
+function handleOperationClick(evt) {
+  operator = evt.currentTarget.innerText;
+  console.log(operator);
+}
+
+
+/** Create equal/evaluate button */
+
+const equal = document.querySelector("#equal");
+equal.addEventListener("click", handleOperate);
+
+
+function handleOperate() {
+  let answer = operate();
+  console.log(answer);
+  x = answer;
+  y = undefined;
+  operator = undefined;
+}
+
+/** eval functions */
+
+function add() {
+  return Number(x) + Number(y);
+}
+
+function subtract() {
   return x - y;
 }
 
-function multiply(x, y) {
+function multiply() {
   return x * y;
 }
 
-function divide(x, y) {
+function divide() {
   return x / y;
 }
 
-function operate(x, y, operator) {
+function operate() {
+
   if (operator === "+") {
-    return add(x, y);
+    return add();
   }
 
   if (operator === "-") {
-    return subtract(x, y);
+    return subtract();
   }
 
   if (operator === "*") {
-    return multiply(x, y);
+    return multiply();
   }
 
   if (operator === "/") {
-    return divide(x, y);
+    return divide();
   }
 
 }
 
-function returnAnswer() {
-  return operate(x, y, operator);
-}
